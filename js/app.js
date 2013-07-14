12d3092faf8361d65cef51154014b9b7e775ad09
kyktommy <kyktommy@gmail.com>
2013-07-14 09:16:39 +0800

    working draft

--- js/app.js
+++ js/app.js
@@ -1,15 +1,47 @@
 var client;
 var $upload_btn = $('#upload-btn');
-var $file_textarea = $('#file-textarea');
+var $list_item = $('.list-item');
 
 (function() {
   setupUI();
-  // setupDropbox();
+  setupDropbox();
 })();
 
+function make_list_item(text) {
+  var $li   = $('<li></li>', { class: 'list-item', text: text }),
+    $button = $('<a href="#my-modal" data-target="#my-modal" data-toggle="modal" class="show-qr btn btn-primary pull-right">QR CODE</a>');
+  $button.appendTo($li);
+  return $li;
+}
+
+function make_qr_code(value, cb) {
+  qr.toDataURL(
+    {
+      level: 'H',
+      size: 8,
+      value: value
+    }, 
+    function(error, url) {
+      cb.call(this, url);
+    }
+  );
+}
+
 function setupUI() {
+  
+  $(document).on('click', '.show-qr', function() {
+    var parent = $(this).parent(),
+        value = parent.clone().children().remove().end().text();
+    make_qr_code(value, function(url) {
+      $.fn.SimpleModal({
+        title: 'QR CODE',
+        contents: '<img class="qr-code" src="'+url+'"/>',
+      }).showModal();
+    });
+  });
+
   $upload_btn.on('click', function() {
-    var value = $('#file-textarea').val();
+    var value = 0;
     uploadMsgToDropbox(client, value);
   });
 };
@@ -18,7 +50,7 @@ function setupDropbox() {
   client = new Dropbox.Client({
     key: "rBn1hErlg2A=|AT99wYOsT1ROW+t9CZRO7fyQ0Gx450jQPbeA/RoMlg==", sandbox: true
   });
-  client.authDriver(new Dropbox.Drivers.Redirect());
+  client.authDriver(new Dropbox.Drivers.Redirect({rememberUser: true}));
   client.authenticate({interactive: false}, function(error, client) {
 
     if (client.isAuthenticated()) { 
@@ -34,9 +66,23 @@ function setupDropbox() {
 
 function start(client) {
   updateInfo(client);
+  downloadMsgFromDropbox(client);
+}
+
+function downloadMsgFromDropbox(client) {
   client.readFile("/msg1.txt", function(error, data) {
-    var $file_textarea = $('#file-textarea');
-    $file_textarea.text(data);
+    if (error && error.response.error == "File has been deleted") {
+      // Create new file if no file exist
+      uploadMsgToDropbox(client, 'First Use, thanks');
+      downloadMsgFromDropbox(client);
+    }
+    else {
+      // Add list item to list
+      var lines = data.split('\n');
+      $.each(lines, function(index, line) {
+        make_list_item(line).appendTo('.list');  
+      });
+    }
   });
 }
 
@@ -49,13 +95,11 @@ function uploadMsgToDropbox(client, value) {
 
 function updateInfo(client) {
   client.getUserInfo(function(error, info) {
-    console.log(info);
     var $db_name = $('#db-name'),
-  $db_filename = $('#db-filename');
+    $db_filename = $('#db-filename');
 
-  $db_name.text( info.name );
-  $db_filename.text( "/msg1.txt" );
-
+    $db_name.text( info.name );
+    $db_filename.text( "/msg1.txt" );
   });
 }
 
