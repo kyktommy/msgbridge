4c99acea626c2bba9d38193bb871f99f8721c636
kyktommy <kyktommy@gmail.com>
2013-07-20 14:47:15 +0800

    working site

--- js/app.js
+++ js/app.js
@@ -2,105 +2,148 @@ var client;
 var $upload_btn = $('#upload-btn');
 var $list_item = $('.list-item');
 
-(function() {
-  setupUI();
-  setupDropbox();
-})();
+var app = angular.module('app', []);
+app.run(function($rootScope, DropboxService) {
+  Loading.show();
+  DropboxService.setupUI();
+  DropboxService.setupDropbox($rootScope);
+  Loading.hide();
+});
 
-function make_list_item(text) {
-  var $li   = $('<li></li>', { class: 'list-item', text: text }),
-    $button = $('<a href="#my-modal" data-target="#my-modal" data-toggle="modal" class="show-qr btn btn-primary pull-right">QR CODE</a>');
-  $button.appendTo($li);
-  return $li;
-}
+app.controller('ListCtrl', function($scope) {
+  $scope.name = "Guest";
+  $scope.file_name = "None";
+  $scope.list = [];
+  $scope.input_text = "";
+
+  $scope.addItem = function() {
+    if($scope.input_text.length != 0) {
+      $scope.list.unshift({name: $scope.input_text});
+    }
+    $scope.input_text = ""
+  };
+
+  $scope.clearList = function() {
+    $scope.list = [];
+  };
+
+  $scope.uploadList = function() {
+    var value = "";
+    $.each($scope.list, function(index, item) {
+      value += item.name + "\n";
+    });
+    uploadMsgToDropbox($scope.client, value);
+  };
+
+  $scope.$on('init', function(event, client) {
+    $scope.client = client;
+    updateInfo(client);
+    downloadMsgFromDropbox(client);
+  });
+
+
+  var downloadMsgFromDropbox = function(client) {
+    client.readFile("/msg1.txt", function(error, data) {
+      if (error && error.response.error == "File has been deleted") {
+        // Create new file if no file exist
+        uploadMsgToDropbox(client, 'First Use, thanks');
+        downloadMsgFromDropbox(client);
+      }
+      else {
+        // Add list item to list
+        var lines = data.split('\n');
+        $.each(lines, function(index, line) {
+          if(line.trim() != "") {
+            $scope.list.unshift({name: line});
+          }
+        });
+        $scope.$apply();
+      }
+    });
+  };
+
+  var uploadMsgToDropbox = function(client, value) {
+    Loading.show();
+    client.writeFile('/msg1.txt', value, 
+        function(error, info) {
+          alert('uploaded'); 
+        });
+    Loading.hide();
+  };
+
+  var updateInfo = function(client) {
+    client.getUserInfo(function(error, info) {
+      $scope.name = info.name;
+      $scope.file_name = "/msg1.txt";
+    });
+  };
+
+});
 
 function make_qr_code(value, cb) {
   qr.toDataURL(
-    {
-      level: 'H',
-      size: 8,
-      value: value
-    }, 
-    function(error, url) {
-      cb.call(this, url);
-    }
-  );
+      {
+        level: 'H',
+    size: 8,
+    value: value
+      }, 
+      function(error, url) {
+        cb.call(this, url);
+      }
+      );
 }
 
-function setupUI() {
-  
-  $(document).on('click', '.show-qr', function() {
-    var parent = $(this).parent(),
+// end list controller
+//
+app.factory('DropboxService', function() {
+
+  return {
+    setupUI: function() {
+
+      $(document).on('click', '.show-qr', function() {
+        var parent = $(this).parent(),
         value = parent.clone().children().remove().end().text();
-    make_qr_code(value, function(url) {
-      $.fn.SimpleModal({
-        title: 'QR CODE',
-        contents: '<img class="qr-code" src="'+url+'"/>',
-      }).showModal();
-    });
-  });
+        make_qr_code(value, function(url) {
+          $.fn.SimpleModal({
+            title: 'QR CODE',
+            contents: '<img class="qr-code" src="'+url+'"/>',
+          }).showModal();
+          $('#simple-modal').css('top', '50px');
+        });
+      });
 
-  $upload_btn.on('click', function() {
-    var value = 0;
-    uploadMsgToDropbox(client, value);
-  });
+    },
+
+      setupDropbox: function($rootScope) {
+        var self = this;
+        client = new Dropbox.Client({
+          key: "rBn1hErlg2A=|AT99wYOsT1ROW+t9CZRO7fyQ0Gx450jQPbeA/RoMlg==", sandbox: true
+        });
+        client.authDriver(new Dropbox.Drivers.Redirect({rememberUser: true}));
+        client.authenticate({interactive: false}, function(error, client) {
+          if (client.isAuthenticated()) { 
+            self.start($rootScope, client);
+          }
+          else { 
+            client.authenticate(function(error, client) {
+              self.start($rootScope, client);
+            });
+          }
+        });
+      },
+
+      start: function($rootScope, client) {
+        $rootScope.$broadcast("init", client);
+      }
+  }
+
+});
+
+var Loading = {
+  show: function(text) {
+    $('.loading').fadeIn();
+  },
+  hide: function() {
+    $('.loading').fadeOut();
+  }
 };
-
-function setupDropbox() {
-  client = new Dropbox.Client({
-    key: "rBn1hErlg2A=|AT99wYOsT1ROW+t9CZRO7fyQ0Gx450jQPbeA/RoMlg==", sandbox: true
-  });
-  client.authDriver(new Dropbox.Drivers.Redirect({rememberUser: true}));
-  client.authenticate({interactive: false}, function(error, client) {
-
-    if (client.isAuthenticated()) { 
-      start(client);
-    }
-    else { 
-      client.authenticate(function(error, client) {
-        start(client);
-      });
-    }
-  });
-}
-
-function start(client) {
-  updateInfo(client);
-  downloadMsgFromDropbox(client);
-}
-
-function downloadMsgFromDropbox(client) {
-  client.readFile("/msg1.txt", function(error, data) {
-    if (error && error.response.error == "File has been deleted") {
-      // Create new file if no file exist
-      uploadMsgToDropbox(client, 'First Use, thanks');
-      downloadMsgFromDropbox(client);
-    }
-    else {
-      // Add list item to list
-      var lines = data.split('\n');
-      $.each(lines, function(index, line) {
-        make_list_item(line).appendTo('.list');  
-      });
-    }
-  });
-}
-
-function uploadMsgToDropbox(client, value) {
-  client.writeFile('/msg1.txt', value, 
-    function(error, info) {
-      alert('uploaded'); 
-  });
-}
-
-function updateInfo(client) {
-  client.getUserInfo(function(error, info) {
-    var $db_name = $('#db-name'),
-    $db_filename = $('#db-filename');
-
-    $db_name.text( info.name );
-    $db_filename.text( "/msg1.txt" );
-  });
-}
-
-
